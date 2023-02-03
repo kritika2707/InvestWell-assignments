ca1c569bada8e3ffdfd2e35e3b42eb36341d9257
kritika2707 <kritika.14052@gnindia.dronacharya.info>
2023-02-03 14:18:25 +0530

    Get method using node.js

--- script.js
+++ script.js
@@ -115,3 +115,27 @@ function toVerify(){
         alert("Please Signup first");
     }
 }
+
+// code for creating server
+const express = require('express'); // Importing express module
+
+const app = express(); // Creating an express object
+
+const port = 8000; // Setting an port for this application
+
+
+// Starting server using listen function
+app.listen(port, function (err) {
+if(err){
+	console.log("Error while starting server");
+}
+else{
+	console.log("Server has been started at "+port);
+}
+})
+
+app.get('/homepage/form/detail', function (req, res) {
+    let form_detail = req.query
+    console.log("Form details:",req.query)
+    res.send(form_detail);
+  })
